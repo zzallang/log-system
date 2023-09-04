f775a2b88f050716028f0424446e7a17a6443240
zzallang <wmfsp9@naver.com>
2023-09-04 19:33:59 +0900

    /

---
+++ src/main/java/com/pptware/logstudy/LogStudyApplication.java
@@ -0,0 +1,13 @@
+package com.pptware.logstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pptware/logstudy/LogStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pptware.logstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
